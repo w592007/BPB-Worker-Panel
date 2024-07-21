6439ad3bbadd19a4e460584348e0a78be29b18a9
w592007 <446878944@qq.com>
2024-07-21 21:30:10 +0800

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -11,7 +11,7 @@ let userID = 'afcd3b07-60bd-46d8-9753-5770b538fd5f';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', '119.28.59.141', '141.147.190.128'];
+const proxyIPs= ['141.147.190.128'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
